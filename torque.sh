dd4191c40557efe0972246d76d1cb55548f16a67
Curso fisi4028  je.forero 2018-09-26 <fisi4028@clustergate.local>
2018-11-14 07:05:06 -0500

    f

--- torque.sh
+++ torque.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=8, mem=64gb
-#PBS -l walltime=02:00
+#PBS -l nodes=1:ppn=8,mem=64gb,walltime=02:00
 #PBS -M a.martinez@uniandes.edu.co
 #PBS -m abe
 #PBS -N Programa
 
-cd /hpcfs/home/fisi4028/a.martinez/AndresMartinez_Ejercicio27
+#cd /hpcfs/home/fisi4028/a.martinez/AndresMartinez_Ejercicio27
+cd $PBS_O_WORKDIR
 make -f make.mk
 ./Listings1
 ./Listings2
